27179424a92d49a2a28bc11ee382ada5ce77f3d2
pavlenty <impetus@yandex.ru>
2020-02-07 21:31:00 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,84 @@
+package com.company;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private List<Player> players = new ArrayList<>();
+
+    public void autoFull() {
+        players.add(new Player("Hulk", "Zenit", 29, Player.Position.FORWARD));
+        players.add(new Player("Kerzhakov", "Zenit", 32, Player.Position.FORWARD));
+        players.add(new Player("Lodygin", "Zenit", 25, Player.Position.GOALKEEPER));
+        players.add(new Player("Dzyuba", "Zenit", 27, Player.Position.FORWARD));
+        players.add(new Player("Lombaerts", "Zenit", 30, Player.Position.DEFENDER));
+        players.add(new Player("Danny", "Zenit", 32, Player.Position.FORWARD));
+        players.add(new Player("Kerzhakov", "Zenit", 27, Player.Position.GOALKEEPER));
+        players.add(new Player("Musa", "CSKA", 23, Player.Position.FORWARD));
+        players.add(new Player("Akinfeev", "CSKA", 29, Player.Position.GOALKEEPER));
+    }
+
+    public void printAll() {
+        System.out.println("\nAll players");
+        players
+                .stream()
+                .forEach(System.out::println);
+    }
+
+    public void printFilterName(String substr) {
+        System.out.println("\nAll players, which starts with " + substr);
+        players
+                .stream()
+                .filter((s) -> s.getName().startsWith(substr))
+                .forEach(System.out::println);
+    }
+
+    public void printSorted() {
+        System.out.println("\nSorted all players");
+        players
+                .stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    public void printMapConvertTeam(String team) {
+        System.out.println("\nConvert all players");
+        players
+                .stream()
+                .map(player -> player.getCloneWithModifyTeam(team))
+                .forEach(System.out::println);
+    }
+
+    public boolean isAnyInTeam(String team) {
+        return players
+                .stream()
+                .anyMatch((s) -> s.getTeam().equals(team));
+    }
+
+    public long countPlayersInTeam(String team) {
+        return players
+                .stream()
+                .filter((s) -> s.getTeam().equals(team))
+                .count();
+    }
+
+    public static void main(String[] args) {
+        Main lto = new Main();
+        lto.autoFull();
+        lto.printAll();
+        lto.printFilterName("Lo");
+        lto.printSorted();
+        lto.printMapConvertTeam("Krasnodar");
+
+        System.out.println("\nIs team " + "CSKA" + "? " + lto.isAnyInTeam("CSKA"));
+        System.out.println("In team " + "CSKA" + "= " + lto.countPlayersInTeam("CSKA"));
+
+        System.out.println("\nIs team " + "Zenit" + "? " + lto.isAnyInTeam("Zenit"));
+        System.out.println("In team " + "Zenit" + "= " + lto.countPlayersInTeam("Zenit"));
+
+        System.out.println("\nIs team " + "Krasnodar" + "? " + lto.isAnyInTeam("Krasnodar"));
+        System.out.println("In team " + "Krasnodar" + "= " + lto.countPlayersInTeam("Krasnodar"));
+    }
+}
